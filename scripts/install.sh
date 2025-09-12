2da20a18d617083f9183e459f1cf32b0d81b945e
komiya <90894519+misya11p@users.noreply.github.com>
2025-09-12 13:42:36 +0900

    add install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,17 @@
+# Install necessary packages via Homebrew
+## cli tools
+brew install bat
+brew install exa
+brew install trash
+brew install dua-cli
+brew install fzf
+brew install fastfetch
+brew install macmon
+brew install font-0xproto-nerd-font
+brew install zoxide
+brew install pure
+
+## dev tools
+brew install neovim
+brew install uv
+brew install volta
