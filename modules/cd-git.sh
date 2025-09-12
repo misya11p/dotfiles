124060ea95cad2a3e256aec32e12e8a2c1660768
komiya <90894519+misya11p@users.noreply.github.com>
2025-09-12 23:53:56 +0900

    fig bug cd-git

--- modules/cd-git.sh
+++ modules/cd-git.sh
@@ -2,15 +2,16 @@
 
 cd-git() {
   local repo_path
-  repo_path=$(
+  repo_id=$(
     ghq list |
     fzf \
-      --height 60% \
+      --height 40% \
       --reverse \
       --no-sort \
-      --bind 'change:reload(ghq list --full-path {q})' \
-      --preview 'bat -p --color=always --line-range :80 $(ghq root)/{}/README.*'
+      --bind 'change:reload(ghq list {q})' \
+      --preview 'bat -p --color=always --line-range :80 $(ghq root)/{}/README.md'
   )
-  \cd "$repo_path"
+  \cd "$(ghq root)/$repo_id"
 }
+
 alias gd="cd-git"
