2d7f891860b5feb92548a86cba53dcf67f3e41dc
komiya <90894519+misya11p@users.noreply.github.com>
2025-09-12 18:06:25 +0900

    fix filename

---
+++ modules/ghq-rm.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+ghq-rm() {
+  set -e
+
+  repo=$(
+    ghq list |
+    fzf \
+      --height 60% \
+      --reverse \
+      --no-sort \
+      --bind 'change:reload(ghq list --full-path {q})' \
+      --preview 'bat -p --color=always --line-range :80 $(ghq root)/{}/README.*'
+  )
+  [ -z "$repo" ] && return 0
+
+  repo_path=$(ghq root)/$repo
+  rm "$repo_path"
+  echo "Removed $repo_path"
+}
+
+alias gr='ghq-rm'
