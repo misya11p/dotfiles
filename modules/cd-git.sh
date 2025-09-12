81df2edef8a3181bfbf0508294834dd4ee1d8e1d
komiya <90894519+misya11p@users.noreply.github.com>
2025-09-12 18:01:24 +0900

    refactor cd-git

--- modules/cd-git.sh
+++ modules/cd-git.sh
@@ -4,12 +4,12 @@ cd-git() {
   local repo_path
   repo_path=$(
     ghq list |
-      fzf \
-        --height 60% \
-        --reverse \
-        --no-sort \
-        --bind 'change:reload(ghq list --full-path {q})' \
-        --preview 'bat -p --color=always --line-range :80 $(ghq root)/{}/README.*'
+    fzf \
+      --height 60% \
+      --reverse \
+      --no-sort \
+      --bind 'change:reload(ghq list --full-path {q})' \
+      --preview 'bat -p --color=always --line-range :80 $(ghq root)/{}/README.*'
   )
   z "$repo_path"
 }
