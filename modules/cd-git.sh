4ce21ce6b141ab286748edf360e8c84a49076a5a
komiya <90894519+misya11p@users.noreply.github.com>
2025-10-08 23:32:55 +0900

    update: git-cdでzoxide scoreを元にソートするようにした

--- modules/cd-git.sh
+++ modules/cd-git.sh
@@ -1,18 +1,26 @@
 #!/bin/zsh
 
 cd-git() {
-  local repo=$(
-    ghq list |
+  local root repo
+  root="$(ghq root)"
+
+  repo=$(
+    ghq list | \
+    while IFS= read -r r; do
+      p="$root/$r"
+      # zoxide `query -ls <path>` prints lines like "<score>\t<path>"
+      s=$(zoxide query -ls "$p" 2>/dev/null | awk '{print $1}')
+      printf "%s\t%s\n" "${s:-0}" "$r"
+    done | sort -rn -k1,1 | cut -f2- | \
     fzf \
       --height 40% \
       --reverse \
       --no-sort \
-      --bind 'change:reload(ghq list {q})' \
-      --preview 'bat -p --color=always --theme=OneHalfLight --line-range :80 $(ghq root)/{}/README.md'
+      --preview "bat -p --color=always --theme=OneHalfLight --line-range :80 $root/{}/README.md"
   )
 
   if [[ -n "$repo" ]]; then
-    \cd "$(ghq root)/$repo"
+    \cd "$root/$repo"
   fi
 }
 
