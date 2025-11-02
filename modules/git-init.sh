11d6bf6fddda813c7862dbba83368ec75d6c5d53
komiya <90894519+misya11p@users.noreply.github.com>
2025-11-03 00:03:51 +0900

    update: git-init

--- modules/git-init.sh
+++ modules/git-init.sh
@@ -25,7 +25,22 @@ git-init() {
   done
 
   gh repo create "$repo" $visibility
-  ghq-get "$repo"
+  ghq get git@github.com:misya11p/${repo}.git
+  repo_dir="$(ghq root)/github.com/misya11p/${repo}"
+  cd "$repo_dir" || { echo "Failed to cd to $repo_dir"; exit 1; }
+  git checkout -b main
+
+  touch README.md
+  git add README.md
+  git commit -m "Initial commit"
+  git push -u origin main
+
+  if [[ -n "$repo_dir" ]]; then
+    read -r "answer?cd to ${repo_dir}? (Y/n) "
+    if [[ -z "$answer" || "$answer" =~ ^[Yy]$ ]]; then
+      \cd "$repo_dir"
+    fi
+  fi
 }
 
 alias gi="git-init"
