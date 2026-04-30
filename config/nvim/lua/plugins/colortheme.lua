2e6ccf48482c7b4eacb638d8fd6d5d97d4744fb6
misya11p <90894519+misya11p@users.noreply.github.com>
2026-04-30 17:45:21 +0900

    configを追加

---
+++ config/nvim/lua/plugins/colortheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    opts = {
+      style = "light",
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+            exclude = { "**/.git", "**/.DS_Store" },
+          },
+        },
+      },
+    },
+  },
+}
